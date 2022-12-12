b2a7aa5f5b74e3b1a2b9887ad2d2a82e52a6eeb9
Jaskiran Singh <jsingh8986@yahoo.com>
2022-12-12 15:47:47 -0800

    commit tree 0afe308c34ef66ba14bece0bcc04e5bf410f1fbe

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    if (pickupBlock > 42) {
+        return pickupBlock - 42
+    } else {
+        return 42 - pickupBlock
+    }
+}
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start)*264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+    const totalDistance = distanceTravelledInFeet(start, destination)
+    if (totalDistance <= 400) {
+        return 0
+    } else if (totalDistance > 400 && totalDistance <= 2000 ) {
+        return(totalDistance - 400) * .02
+    } else if (totalDistance > 2000 && totalDistance <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
